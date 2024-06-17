a2841a5c32013f397571218128a5e990ad1b509d
TaddOlson <jonathantaddolson95@gmail.com>
2024-06-17 17:37:19 -0500

    Negative powerup player looking work.

--- Assets/Scripts/NegativePowerup.cs
+++ Assets/Scripts/NegativePowerup.cs
@@ -4,27 +4,38 @@ using UnityEngine;
 
 public class NegativePowerup : MonoBehaviour
 {
-    public Transform player;
+    [SerializeField]
+    private Transform _playerTransform;
+    private Player _playerObject;
     private float _speed = 4f;
     private float _maxDist = 10f;
     private float _minDist = 0;
     private AudioClip _clip;
 
+    void Start()
+    {
+        _playerObject = GameObject.Find("Player").GetComponent<Player>();
+
+    }
+
     // Update is called once per frame
     void Update()
     {
         transform.Translate(Vector3.down * _speed * Time.deltaTime);
-        transform.LookAt(player);
+        Vector3 lookVector = _playerTransform.transform.position - transform.position;
+        lookVector.y = transform.position.y;
+        Quaternion rot = Quaternion.LookRotation(lookVector);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rot, 1);
+        transform.LookAt(_playerTransform);
 
-        if (Vector3.Distance(transform.position, player.position) >= _minDist)
+        if (Vector3.Distance(transform.position, _playerTransform.position) >= _minDist)
         {
             transform.position += transform.forward * _speed * Time.deltaTime;
         }
-        else if (Vector3.Distance(transform.position, player.position) <= _maxDist)
+        else if (Vector3.Distance(transform.position, _playerTransform.position) <= _maxDist)
         {
-            
+            transform.position += transform.forward * _speed * Time.deltaTime;
         }
-
     }
 
     public void OnTriggerEnter2D(Collider2D other)
